dbc87973a2f0037c112e249f01367d3b23b945aa
Calvin Le <email.calvinle@gmail.com>
2023-09-03 18:30:53 -0700

    created folders and files

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+
+const App = () => {
+  return <div>3d Developer Portfolio</div>;
+};
+
+export default App;
