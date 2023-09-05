4af7adb5da816bd4faa3b739060735ca49607065
Calvin Le <email.calvinle@gmail.com>
2023-09-04 17:58:20 -0700

    work experience done

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,10 +5,11 @@ import { motion } from "framer-motion";
 import { styles } from "../styles";
 import { services } from "../constants";
 import { fadeIn, textVariant } from "../utils/motion";
+import { SectionWrapper } from "./hoc";
 
 const ServiceCard = ({ index, title, icon }) => {
   return (
-    <Tilt className="xs:w=[250px] w-full">
+    <Tilt className="xs:w-[250px] w-full">
       <motion.div
         variants={fadeIn("right", "spring", 0.5 * index, 0.75)}
         className="w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card"
@@ -32,11 +33,11 @@ const About = () => {
     <>
       <motion.div variants={textVariant}>
         <p className={styles.sectionSubText}>Introduction</p>
-        <h2 className={styles.sectionSubText}>Overview.</h2>
+        <h2 className={styles.sectionHeadText}>Overview.</h2>
       </motion.div>
       <motion.p
         variants={fadeIn("", "", 0.1, 1)}
-        className="mt-4 text-secondary font-[17px] max-w-3xl leading-[30px]"
+        className="mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]"
       >
         A passionate and results-driven{" "}
         <strong>Full Stack Web Developer</strong> and{" "}
@@ -52,4 +53,4 @@ const About = () => {
   );
 };
 
-export default About;
+export default SectionWrapper(About, "about");
